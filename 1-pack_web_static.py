1f5d97a58541538f4f095f8525b977e1897143ca
daorejuela1 <vids@hotmail.es>
2020-08-17 22:58:55 +0000

    added -p to no launch an error when creating

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,9 +10,13 @@ def do_pack():
     """
     function to pack in .tgz
     """
-    local("mkdir versions")
+    local("mkdir -p versions")
     now = datetime.today()
-    file_name = "web_static_{}{}{}{}{}{}".format(now.year, now.month,
-                                                 now.day, now.hour,
-                                                 now.minute, now.second)
-    local("tar -cvzf versions/{}.tgz web_static".format(file_name))
+    try:
+        file_name = "web_static_{}{}{}{}{}{}.tgz".format(now.year, now.month,
+                                                         now.day, now.hour,
+                                                         now.minute, now.second)
+        local("tar -cvzf versions/{} web_static".format(file_name))
+        return (file_name)
+    except:
+        return (None)
