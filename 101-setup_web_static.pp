5e3a5b8f774c28db73ce2f1be5060c51ee7a25c0
daorejuela1 <vids@hotmail.es>
2020-08-18 03:19:30 +0000

    added last advance

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,32 @@
+#This pupppet scripts assure an nginx installation
+
+exec { '/usr/bin/env apt-get -y update':}
+->package {'nginx':
+ensure => installed,
+}
+
+->file { [ '/data/',
+  '/data/web_static/',
+  '/data/web_static/releases/',
+  '/data/web_static/releases/test/',
+  '/data/web_static/shared/', ]:
+  ensure => directory,
+}
+
+->file {'/data/web_static/releases/test/index.html':
+content => 'Holberton School for the win yeah!\n',
+}
+
+-> exec {'/usr/bin/env ln -sf /data/web_static/releases/test/ /data/web_static/current':}
+-> exec {'/usr/bin/env chown -R ubuntu:ubuntu /data/':}
+
+->file_line {'add protocol':
+ensure => present,
+path   => '/etc/nginx/sites-available/default',
+after  => 'listen 80 default_server;',
+line   => 'location /hbnb_static/ { alias /data/web_static/current/; autoindex off;}',
+}
+
+->service { 'nginx':
+ensure     => running,
+}
